dcf86405a3aa138981750c5f55bb26a4f5f08e84
dzhakhar <dzhakhar.ukhaev@gmail.com>
2017-03-05 01:30:49 +0600

    added several routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+var allowedWordLength = 20;
+
 function makeid(){
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -28,6 +30,8 @@ app.get("/", function(req, res){
   res.send("Hello, World!");
 })
 
+// routes
+
 app.get("/getuniquetoken", function(req, res){
   var token = makeid();
 
@@ -36,6 +40,29 @@ app.get("/getuniquetoken", function(req, res){
   res.send(token);
 })
 
+app.get("/allowedwordlength", function(req, res){
+  res.send(allowedWordLength);
+})
+
+app.get("/sendchar", function(req, res){
+  var payload = req.query.payload;
+  var token = req.query.token;
+
+  if(fs.existsSync(token)){
+    fs.writeFileSync(token + "/logs.txt", payload, function(err){
+      if(err){
+        res.send("error");
+      } else {
+        res.send("success");
+      }
+    });
+  } else {
+      res.send("incorrect token");
+  }
+})
+
+
+// running the server
 app.listen(3000, function(){
   console.log("Server listening on port 3000!");
 })
